bd2457de0c76dcee901dfd9615d1cb56957c75ca
Michael Rose <michael.rose0@gmail.com>
2020-03-06 21:33:48 +0000

    Update figure eight trajectory generator.

--- config/traj/MakePeriodicTrajectory.py
+++ config/traj/MakePeriodicTrajectory.py
@@ -22,20 +22,20 @@ with open('FigureEight.txt', 'w') as the_file:
         y = math.sin(t * 2 * math.pi / period[1] + phase[1]) * radius * amp[1] + center[1];
         z = math.sin(t * 2 * math.pi / period[2] + phase[2]) * radius * amp[2] + center[2];
         the_file.write(fmt(t) + "," + fmt(x) + "," + fmt(y) + "," + fmt(z));
-		vx = 0;
-		vy = 0;
-		vz = 0;
-		######## BEGIN STUDENT CODE
-		
-		######## END STUDENT CODE
-		the_file.write("," + fmt(vx) + "," + fmt(vy) + "," + fmt(vz));
-		######## EXAMPLE SOLUTION
+    
+        ######## BEGIN STUDENT CODE
+        vx = 2 * math.pi / period[0] * math.cos(t * 2 * math.pi / period[0] + phase[0]) * radius * amp[0]
+        vy = 2 * math.pi / period[1] * math.cos(t * 2 * math.pi / period[1] + phase[1]) * radius * amp[1]
+        vz = 2 * math.pi / period[2] * math.cos(t * 2 * math.pi / period[2] + phase[2]) * radius * amp[2]
+        ######## END STUDENT CODE
+        the_file.write("," + fmt(vx) + "," + fmt(vy) + "," + fmt(vz));
+        ######## EXAMPLE SOLUTION
         #the_file.write("," + fmt((x-px)/timestep) + "," + fmt((y-py)/timestep) + "," + fmt((z-pz)/timestep));
-		#px = x;
+        #px = x;
         #py = y;
         #pz = z;
-		######## END EXAMPLE SOLUTION
-		
+        ######## END EXAMPLE SOLUTION
+        
         the_file.write("\n");
         
         t += timestep;
